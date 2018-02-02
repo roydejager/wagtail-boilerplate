f4558ba3f84c70d1d31b4e5486ceb5043e82b81a
roydejager <roydejager@live.nl>
2018-02-02 16:55:40 +0100

    add api

--- wagtail_boilerplate/settings/base.py
+++ wagtail_boilerplate/settings/base.py
@@ -50,6 +50,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'wagtail.api.v2',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- wagtail_boilerplate/urls.py
+++ wagtail_boilerplate/urls.py
@@ -4,21 +4,25 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from .api import api_router
+
 from wagtail.wagtailadmin import urls as wagtailadmin_urls
 from wagtail.wagtailcore import urls as wagtail_urls
 from wagtail.wagtaildocs import urls as wagtaildocs_urls
 
 from search import views as search_views
 
+
 urlpatterns = [
     url(r'^django-admin/', include(admin.site.urls)),
 
-    url(r'^', include(wagtailadmin_urls)),
+    url(r'^api/', api_router.urls),
     url(r'^documents/', include(wagtaildocs_urls)),
 
     url(r'^search/$', search_views.search, name='search'),
 
-    # For anything not caught by a more specific rule above, hand over to
+    url(r'^admin/', include(wagtailadmin_urls)),
+    # For anything not caught by a more specifi c rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
     url(r'', include(wagtail_urls)),
